9046dac5540d95ad7603f934cebcaf35805b5286
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2024-11-27 17:36:55 -0500

    first commit

---
+++ src/api/index.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const { VITE_API_URL, VITE_SECRET_KEY } = import.meta.env;
+
+export const scheduleApi = axios.create({
+  baseURL: VITE_API_URL,
+  headers: {
+    "api-key": VITE_SECRET_KEY,
+  },
+});
+scheduleApi.interceptors.request.use(
+  (config: any) => {
+    // Obtener el token directamente del localStorage
+    const jwt = localStorage.getItem("jwt");
+
+    if (jwt) {
+      // Configurar el header de autorización si el token es válido
+      config.headers.Authorization = `Bearer ${jwt}`;
+    }
+
+    return config;
+  },
+  (error: any) => {
+    return Promise.reject(error);
+  }
+);
