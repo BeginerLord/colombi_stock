a63ecce369e465629d53490985cf0022ca0f3f81
rafapsjs <rafaelcorredorgambin1@gmail.com>
2024-11-28 14:30:58 -0500

    fixxes the settings the api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,7 @@
 import axios from "axios";
 
-const { VITE_API_URL, VITE_SECRET_KEY } = import.meta.env;
+const { VITE_API_URL } = import.meta.env;
 
 export const scheduleApi = axios.create({
-  baseURL: VITE_API_URL,
-  headers: {
-    "api-key": VITE_SECRET_KEY,
-  },
+  baseURL: VITE_API_URL
 });
-scheduleApi.interceptors.request.use(
-  (config: any) => {
-    // Obtener el token directamente del localStorage
-    const jwt = localStorage.getItem("jwt");
-
-    if (jwt) {
-      // Configurar el header de autorización si el token es válido
-      config.headers.Authorization = `Bearer ${jwt}`;
-    }
-
-    return config;
-  },
-  (error: any) => {
-    return Promise.reject(error);
-  }
-);
