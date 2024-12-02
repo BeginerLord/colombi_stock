d510bc4122193d2e9c9a3dadbe031e511c8030f5
NelssRUix <nelrum2003uc@gmail.com>
2024-12-02 09:26:55 -0500

    add feature stock movement date range

--- src/services/stockMovement/index.ts
+++ src/services/stockMovement/index.ts
@@ -49,10 +49,28 @@ export const GetAllStockMovements = async (
   size: number = 10,
   sortBy: string = "movementDate",
   direction: string = "asc",
-  status: "ACTIVE" | "INACTIVE",
+  statusEntity: "ACTIVE" | "INACTIVE",
   movementType: "STOCK_IN" | "STOCK_OUT"
 ) => {
-  const url = `/stock_movement/movement?page=${page}&size=${size}&sortBy=${sortBy}&direction=${direction}&status=${status}&movementType=${movementType}`;
+  const url = `/stock_movement/movement?page=${page}&size=${size}&sortBy=${sortBy}&direction=${direction}&status=${statusEntity}&movementType=${movementType}`;
+
+  const { data } = await scheduleApi.get(url);
+
+  return data as PaginatedResponse<StockMovementModelDto>;
+};
+
+
+
+export const GetStockMovementsByDateRange = async (
+  page: number = 0,
+  size: number = 10,
+  sortBy: string = "movementDate",
+  direction: string = "asc",
+  statusEntity: 'ACTIVE' | 'INACTIVE',
+  startDate: string,
+  endDate: string
+) => {
+  const url = `/stock_movement/date-range?page=${page}&size=${size}&sortBy=${sortBy}&direction=${direction}&status=${statusEntity}&startDate=${startDate}&endDate=${endDate}`;
 
   const { data } = await scheduleApi.get(url);
 
