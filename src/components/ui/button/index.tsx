e38015506815e030749d0dc5cf252f8b3bfee460
BeginerLord <133072912+BeginerLord@users.noreply.github.com>
2024-12-01 22:47:31 -0500

    add category registration form and related hooks

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -2,18 +2,12 @@ import { Button } from "@mui/material";
 interface PropsButton {
   name: string;
   type?: "button" | "submit" | "reset";
-
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const ButtonComponet = ({ name, onClick, type = "button" }: PropsButton) => {
+const ButtonComponet = ({ name, type = "button" }: PropsButton) => {
   return (
     <>
-      <Button
-type={type}        variant="contained"
-        color="primary"
-        onClick={onClick}
-      >
+      <Button type={type} variant="contained" color="primary">
         {name}
       </Button>
     </>
