7a7e77c71764735d2976917c10ed976fc4880a2f
rafapsjs <rafaelcorredorgambin1@gmail.com>
2024-12-02 10:17:19 -0500

    fixxes erro

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -12,7 +12,6 @@ const ButtonComponet = ({ name, type = "button" }: PropsButton) => {
       <Button style={{ fontSize: '1.4rem', padding: '0.7rem 1.7rem' }}
 type={type}        variant="contained"
         color="primary"
-        onClick={onClick}
       >
  
         {name}
