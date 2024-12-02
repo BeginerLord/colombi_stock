44f4602ab805646c73987b0724ae89b4dcaab1fc
rafapsjs <rafaelcorredorgambin1@gmail.com>
2024-12-01 20:32:48 -0500

    add the module feature suppliers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterSuppliers from "./components/uiSuppliers"; // Adjust the import path as needed
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/xd" element={<RegisterSuppliers />} />
+        <Route path="*" element={<div>Page not found</div>} /> {/* Default route */}
       </Routes>
     </BrowserRouter>
   );
